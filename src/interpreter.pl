6a4936287651948daf1f3eca6b688d6e141dc51c
shloka23 <spandy15@asu.edu>
2024-04-21 22:41:13 -0700

    add(interpretor): print, declr and if else commands added

--- src/interpreter.pl
+++ src/interpreter.pl
@@ -9,12 +9,68 @@ eval_cmd_list(cmd_list_node(Command, Commands), Env, NewEnv) :-
 eval_cmd_list(cmd_node(Command), Env, NewEnv) :-
     eval_cmd(Command, Env, NewEnv).
 
-/*eval_cmd(Command, Env, NewEnv) :- declration_assignment_command(Command,Env,NewEnv).
-eval_cmd(Command, Env, NewEnv) :- print_command(Command,Env,NewEnv).
-eval_cmd(Command, Env, NewEnv) :- if_command(Command,Env,NewEnv).
-eval_cmd(Command, Env, NewEnv) :- if_else_command(Command,Env,NewEnv).
-eval_cmd(Command, Env, NewEnv) :- if_elseif_else_command(Command,Env,NewEnv).
-eval_cmd(Command, Env, NewEnv) :- forloop_command(Command,Env,NewEnv).
+/* 
+ * DECLARATION AND ASSIGNMENT COMMAND
+ */
+eval_cmd(assignment(variable_name(VarName), E1),Env, NewEnv) :- 
+                                    eval_expression(E1, Env, R1), 
+                                    update(VarName, R1, Env, NewEnv).
+eval_cmd(declration_assignment(variable_type(Type),variable_name(VarName), E1), Env, NewEnv) :- 
+                                    eval_expression(E1, Env, R1), 
+                                    update(Type, VarName, R1, Env, NewEnv).
+
+%when declaration setting up default value which is empty @Shloka
+eval_cmd(declaration(variable_type(Type), variable_name(Name)), Env, NewEnv) :- update(Type, Name, _, Env, NewEnv).
+
+/* 
+ * PRINT COMMAND - eval_cmd(print(Expression), Env, Env)
+ */ 
+eval_cmd(print(Expression), Env, Env) :- eval_expression(Expression, Env, Result), write(Result), nl.
+
+/* 
+ * IF COMMAND - eval_cmd(if(cond,block), Env, NewEnv)
+ */ 
+eval_cmd(if(Condition,Block), Env, NewEnv) :- eval_condition(Condition,Env,true), 
+    								eval_block(Block, Env, NewEnv).
+eval_cmd(if(Condition,Block), Env, Env) :- eval_condition(Condition,Env,false).
+
+/* 
+ * IF ELSE COMMAND - eval_cmd(if_else(cond,block1,block2), Env, NewEnv)
+ */ 
+
+eval_cmd(if_else(Condition,Block1,_Block2), Env, NewEnv) :- eval_condition(Condition,Env,true),
+    								eval_block(Block1, Env, NewEnv).
+eval_cmd(if_else(Condition,_Block1,Block2), Env, NewEnv) :- eval_condition(Condition,Env,false),
+    								eval_block(Block2, Env, NewEnv).
+
+/* 
+ * IF ELSEIF ELSE COMMAND - eval_cmd(if_elseif_else(cond,block1,block2), Env, NewEnv)
+ */ 
+
+eval_cmd(if_elseif_else(Condition, Block1,_ElIfCondition, _Block2), Env, NewEnv) :- 
+    						eval_condition(Condition,Env,true),
+    						eval_block(Block1, Env, NewEnv).
+
+eval_cmd(if_elseif_else(Condition,Block1, ElIfCondition, _Block2), Env, NewEnv) :-
+    							eval_condition(Condition,Env,false),
+    							eval_elseif(Block2, Env, NewEnv,true).
+
+eval_cmd(if_elseif_else(Condition,Block1, ElIfCondition, Block2), Env, NewEnv) :-
+    							eval_condition(Condition,Env,false),
+    							eval_elseif(Block2, Env, Env,false),
+    							exal_block(Block2,Env, NewEnv).
+
+eval_elseif(elseif(ElIfCondition, Block), Env, NewEnv, true) :- eval_condition(ElIfCondition,Env,true), 
+    								eval_block(Block, Env, NewEnv).
+eval_elseif(elseif(ElIfCondition, Block), Env, Env, false) :- eval_condition(ElIfCondition,Env,false).
+
+eval_condition(t_condition(E1, Operator, E2), Env, Result) :-
+    eval_expression(E1, Env, R1),
+    eval_expression(E2, Env, R2),
+    eval_comparison(R1, Operator, R2, Result).
+
+
+/*eval_cmd(Command, Env, NewEnv) :- forloop_command(Command,Env,NewEnv).
 eval_cmd(Command, Env, NewEnv) :- whileloop_command(Command,Env,NewEnv).
 eval_cmd(Command, Env, NewEnv) :- compact_forloop_command(Command,Env,NewEnv).
 eval_cmd(Command, Env, NewEnv) :- list_command(Command,Env,NewEnv).*/
@@ -32,7 +88,7 @@ eval_expression(div(E1,E2), Env, Result) :- eval_expression(E1, Env, Numerator),
 eval_expression(bool(B),_, B).
 eval_expression(int(Value),_, Value).
 eval_expression(float(Value),_, Value).
-eval_expression(sting(Value),_, Value).
+eval_expression(string(Value),_, Value).
 eval_expression(variable_name(Name),Env, Value) :- member((_,Name, Value),Env).
 
 eval_expression(increment(VarName), Env, NewEnv) :- lookup(VarName, Env, Val), 
@@ -51,10 +107,10 @@ eval_expression(bool_expr(E1, bool_op(Operator), E2), Env, Result) :-
 eval_bool(true, true).
 eval_bool(false,true).
 
-%@SHLOKA figure out how to incorporate !(Expression)
+%@SHLOKA figure out how to incorporate !!(Expression)
 eval_bool(bool_not(X), Result) :- eval_bool(X, Result1), not(Result1,Result).
 
-%AND, OR
+%AND,OR
 eval_bool(true, &&, true, true).
 eval_bool(true, &&, false, false).
 eval_bool(false, &&, true, false).
@@ -67,9 +123,18 @@ eval_bool(false, '||', false, false).
 
 %comparisons
 eval_comparison(Val1, >, Val2, true)  :- Val1 > Val2.
-eval_comparison(Val1, >, Val2, false)  :- Val1=< Val2.
+eval_comparison(Val1, >, Val2, false)  :- Val1 =< Val2.
 eval_comparison(Val1, <, Val2, true)  :- Val1 < Val2.
 eval_comparison(Val1, <, Val2, false)  :- Val1 >= Val2.
+eval_comparison(Val1, >=, Val2, true)  :- Val1 >= Val2.
+eval_comparison(Val1, >=, Val2, false)  :- Val1 < Val2.
+eval_comparison(Val1, <=, Val2, true)  :- Val1 =< Val2.
+eval_comparison(Val1, <=, Val2, false)  :- Val1 > Val2.
+eval_comparison(Val1, ==, Val2, true)  :- Val1 =:= Val2.
+eval_comparison(Val1, ==, Val2, false)  :- Val1 =\= Val2.
+eval_comparison(Val1, "!!=", Val2, true)  :- Val1 =\= Val2.
+eval_comparison(Val1, "!!=", Val2, false)  :- Val1 =:= Val2.
+
 
 %negates the bool
 not(true, false).
@@ -91,9 +156,9 @@ update(VarName, Value, [(string, VarName,_)|Tail], [(int, VarName, Value)|Tail])
 update(VarName, Value, [H|Tail], [H|UpdatedTail]) :- H \= (_,VarName,_), update(VarName, Value, Tail, UpdatedTail). 
 
 /* 2. update(Type,VarName, Value, Env, NewEnv) */
-update(Type,VarName, Value, [], [Type, VarName, Value]).
+update(Type, VarName, Value, [], [Type, VarName, Value]).
 %should not allow to redine the same var twice, regardless of the type and Env remians the same
-update(Type, VarName, Value, [(_, VarName, _)| Tail], [(Type, VarName, Value)| Tail]). %add error @Shloka
+update(Type, VarName, Value, [(if_else_command, VarName, _)| Tail], [(Type, VarName, Value)| Tail]). %add error @Shloka
 update(Type, VarName, Value, [H|Tail], [H|UpdatedTail]) :- H \= (_,VarName,_), update(Type, VarName, Value, Tail, UpdatedTail). 
 
 
