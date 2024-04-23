872dcac36c34b207953e462b040a34b5e4c306a7
Malavika Anand <msanand@asu.edu>
2024-04-22 20:43:26 -0700

    Completed Lexer.py and added comments

--- src/lexer.py
+++ src/lexer.py
@@ -1,13 +1,15 @@
+# Importing necessary modules
 import ply.lex as lex
 import os
 import sys
 import argparse
 
+# Class for defining colors for console output
 class Colors:
-    GREEN = '\033[92m'
-    DEFAULT = '\033[0m'
-    BLUE = '\033[34m'
-    POODLE_TOKENS_EXTENSION = '.pootokens'
+    GREEN = '\033[92m'  # Green color code
+    DEFAULT = '\033[0m'  # Default color code
+    BLUE = '\033[34m'    # Blue color code
+    POODLE_TOKENS_EXTENSION = '.pootokens'  # File extension for tokens
 
 # All the Tokens used in Poodle
 tokens = (
@@ -18,88 +20,92 @@ tokens = (
 )
 
 # Regular expression rules for simple tokens
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_MULTIPLY = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_DECREMENT = r'\--'
-t_INCREMENT = r'\++'
-t_FLOAT = r'\d+\.\d+'
-t_GREATER_EQUAL = r'>='
-t_GREATER = r'>'
-t_LESSER_EQUAL = r'<='
-t_LESSER = r'<'
-t_MODULO = r'%'
-t_NOT_EQUAL = r'!!='
-t_NUMBER = r'\d+'
-t_POWER = r'\^'
-t_SINGLE_QUOTES = r'\''
-t_ASSIGN = r'='
-t_STRING = r'\".*?\"'
+t_PLUS = r'\+'           # Plus operator
+t_MINUS = r'-'           # Minus operator
+t_MULTIPLY = r'\*'       # Multiply operator
+t_DIVIDE = r'/'          # Divide operator
+t_LPAREN = r'\('         # Left parenthesis
+t_RPAREN = r'\)'         # Right parenthesis
+t_DECREMENT = r'\--'     # Decrement operator
+t_INCREMENT = r'\++'     # Increment operator
+t_FLOAT = r'\d+\.\d+'   # Floating point number
+t_GREATER_EQUAL = r'>=' # Greater than or equal to
+t_GREATER = r'>'        # Greater than
+t_LESSER_EQUAL = r'<='  # Lesser than or equal to
+t_LESSER = r'<'         # Lesser than
+t_MODULO = r'%'         # Modulo operator
+t_NOT_EQUAL = r'!!='    # Not equal to
+t_NUMBER = r'\d+'       # Integer number
+t_POWER = r'\^'         # Power operator
+t_SINGLE_QUOTES = r'\'' # Single quotes
+t_ASSIGN = r'='         # Assignment operator
+t_STRING = r'\".*?\"'   # String enclosed in double quotes
 
 # Regex for Tokens with Action Code
 def t_IDEN(t):
-    r'[a-zA-Z][a-zA-Z0-9_]*'
+    r'[a-zA-Z][a-zA-Z0-9_]*'  # Identifier
     return t
 
 # Regex for Special Symbols
-t_LCURL = r'\{'
-t_RCURL = r'\}'
-t_COMMA = r','
-t_QMARK = r'\?'
-t_SEMICOLON = r';'
-t_COLON = r':'
-t_LSQUARE = r'\['
-t_RSQUARE = r'\]'
-t_PERIOD = r'\.'
-t_EXCLAMATION = r'!'
+t_LCURL = r'\{'          # Left curly brace
+t_RCURL = r'\}'          # Right curly brace
+t_COMMA = r','           # Comma
+t_QMARK = r'\?'          # Question mark
+t_SEMICOLON = r';'       # Semicolon
+t_COLON = r':'           # Colon
+t_LSQUARE = r'\['        # Left square bracket
+t_RSQUARE = r'\]'        # Right square bracket
+t_PERIOD = r'\.'         # Period
+t_EXCLAMATION = r'!'     # Exclamation mark
 
 # Error handling rule
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
-    t.lexer.skip(1)
+    print(f"Illegal character '{t.value[0]}'")  # Print error message
+    t.lexer.skip(1)  # Skip the illegal character
 
 # Build the lexer
 lexer = lex.lex()
 
+# Function to parse command line arguments
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Poodle Lexer Converts the Source Code into Tokens.')
+        description='Poodle Lexer Converts the Source Code into Tokens.')  # Description for argparse
     parser.add_argument('input', metavar='InputFileName', type=str,
-                        nargs=1, help='POODLE Source Code Path')
-    parser.add_argument('--evaluate', action='store_true', help='Analyze the Source Code')
-    return parser.parse_args()
+                        nargs=1, help='POODLE Source Code Path')  # Input filename argument
+    parser.add_argument('--evaluate', action='store_true', help='Analyze the Source Code')  # Optional evaluate flag
+    return parser.parse_args()  # Parse arguments and return them
 
+# Function to read input file
 def read_input_file(filename):
     data = None
     try:
         with open(filename, "r") as input_file:
-            data = input_file.read()
+            data = input_file.read()  # Read file content
     except FileNotFoundError:
-        print("File Does Not Exist: ", sys.argv[1])
-    print("Source Code: " + Colors.GREEN + 'Read Successfully' + Colors.DEFAULT)
-    return data
+        print("File Does Not Exist: ", sys.argv[1])  # Print error if file not found
+    print("Source Code: " + Colors.GREEN + 'Read Successfully' + Colors.DEFAULT)  # Print success message
+    return data  # Return file content
 
+# Function to write tokens to file
 def write_tokens_to_file(tokens, filename):
     with open(filename, "w") as file:
         for tok in tokens:
-            file.write('{}\t'.format(tok.value))
+            file.write('{}\t'.format(tok.value))  # Write token value to file
         print("Tokens Written in " + filename + ": " + Colors.GREEN +
-              'Writing Successful' + Colors.DEFAULT)
+              'Writing Successful' + Colors.DEFAULT)  # Print success message
 
+# Main function
 if __name__ == '__main__':
-    print(Colors.BLUE + "Starting POODLE Lexer" + Colors.DEFAULT)
-    parsed_args = parse_args()
-    input_filename = parsed_args.input[0]
-    output_filename = parsed_args.input[0][:-4] + Colors.POODLE_TOKENS_EXTENSION
-    file_data = read_input_file(input_filename)
+    print(Colors.BLUE + "Starting POODLE Lexer" + Colors.DEFAULT)  # Print starting message in blue
+    parsed_args = parse_args()  # Parse command line arguments
+    input_filename = parsed_args.input[0]  # Get input filename from arguments
+    output_filename = parsed_args.input[0][:-4] + Colors.POODLE_TOKENS_EXTENSION  # Generate output filename
+    file_data = read_input_file(input_filename)  # Read input file
 
-    lexer.input(file_data)
-    tokens = list(lexer)
-    write_tokens_to_file(tokens, output_filename)
+    lexer.input(file_data)  # Input data to lexer
+    tokens = list(lexer)  # Get list of tokens
+    write_tokens_to_file(tokens, output_filename)  # Write tokens to file
 
-    should_evaluate = parsed_args.evaluate
+    should_evaluate = parsed_args.evaluate  # Check if evaluate flag is set
     if should_evaluate:
-        os.system("swipl -g \"main('" + output_filename + "')\" main.pl")
+        os.system("swipl -g \"main('" + output_filename + "')\" main.pl")  # Execute evaluation command
