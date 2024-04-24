6236a18f4a2938410e2f4ac756450ed9bbd9b5d2
Malavika Anand <msanand@asu.edu>
2024-04-23 18:08:53 -0700

    Updated mainFile.pl with Token Reader

--- src/mainFile.pl
+++ src/mainFile.pl
@@ -1,47 +1,51 @@
-:- module(file_reader, [read_file/2]).
+:- module(custom_file_reader, [read_custom_file/2]).
 
-% Predicate for reading a file and converting its contents
-read_file(FileName, ConvertedData) :-
-    % Open the file and read its contents
-    open_file(FileName, Stream),
-    read_lines(Stream, FileLines),
-    close_file(Stream),
-    % Convert atom elements to numbers if possible
-    convert_atoms_to_numbers(FileLines, ConvertedData).
+% read_custom_file/2 reads a file and converts its contents into a list of atoms.
+% It takes the file name as input and returns the converted data.
+read_custom_file(FileName, ConvertedData) :-
+    % Open the file in read mode
+    open_custom_file(FileName, Stream),
+    % Read lines from the stream and convert them to a list of atoms
+    read_custom_lines(Stream, FileData),
+    % Close the stream
+    close_custom_file(Stream),
+    % Convert atoms to numbers if possible
+    convert_atoms_custom(FileData, ConvertedData).
 
-% Open the file and create a stream
-open_file(FileName, Stream) :-
+% open_custom_file/2 opens a file and creates a stream.
+open_custom_file(FileName, Stream) :-
     open(FileName, read, Stream).
 
-% Read lines from the stream and convert them to atoms
-read_lines(Stream, [Line | RestLines]) :-
+% read_custom_lines/2 reads lines from the stream and converts them to a list of atoms.
+read_custom_lines(Stream, [CurrentLineChars | RestChars]) :-
+    % Check if not at the end of the stream
+    \+ at_end_of_custom_stream(Stream),
     % Read a line from the stream and convert it to an atom
-    read_line(Stream, Line),
+    read_line_to_codes(Stream, LineCodes),
+    atom_codes(CurrentLineChars, LineCodes),
     % Recursively read the next line
-    read_lines(Stream, RestLines).
-read_lines(_, []). % Base case: end of stream
+    read_custom_lines(Stream, RestChars),
+    !.
 
-% Read a line from the stream and convert it to an atom
-read_line(Stream, Atom) :-
-    \+ at_end_of_stream(Stream), % Check if not at the end of the stream
-    read_line_to_codes(Stream, Codes), % Read line as a list of character codes
-    atom_codes(Atom, Codes), % Convert the codes to an atom
-    !. % Cut to prevent backtracking
-read_line(_, []). % Base case: end of stream
+read_custom_lines(_, []). % Base case: end of stream
 
-% Convert atom elements to numbers if possible
-convert_atoms_to_numbers([Atom | RestAtoms], [Number | RestNumbers]) :-
+% at_end_of_custom_stream/1 checks if the stream is at the end.
+at_end_of_custom_stream(Stream) :-
+    at_end_of_stream(Stream).
+
+% convert_atoms_custom/2 converts atom elements to numbers if possible.
+convert_atoms_custom([Atom | RestAtoms], [Number | RestNumbers]) :-
     % Convert atom to number if possible
-    atom_to_number_if_possible(Atom, Number),
+    atom_to_number_custom(Atom, Number),
     % Recursively convert the rest of the atoms
-    convert_atoms_to_numbers(RestAtoms, RestNumbers).
-convert_atoms_to_numbers([Atom | RestAtoms], [Atom | RestConverted]) :-
+    convert_atoms_custom(RestAtoms, RestNumbers).
+convert_atoms_custom([Atom | RestAtoms], [Atom | RestConverted]) :-
     % Leave the atom unchanged if it cannot be converted to a number
-    convert_atoms_to_numbers(RestAtoms, RestConverted).
-convert_atoms_to_numbers([], []). % Base case: empty list
+    convert_atoms_custom(RestAtoms, RestConverted).
+convert_atoms_custom([], []). % Base case: empty list
 
-% Convert atom to number if possible
-atom_to_number_if_possible(Atom, Number) :-
-    atom_number(Atom, Number), % Convert atom to number if possible
+% atom_to_number_custom/2 converts an atom to a number if possible.
+atom_to_number_custom(Atom, Number) :-
+    atom_number(Atom, Number),
     !. % Cut to prevent backtracking
-atom_to_number_if_possible(Atom, Atom). % Leave the atom unchanged if it cannot be converted
+atom_to_number_custom(Atom, Atom). % Leave the atom unchanged if it cannot be converted
